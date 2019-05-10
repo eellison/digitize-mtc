499446cfa044c3e57797a5dc877ac43f647343cf
Sudheesha Perera <sudheeshaperera@gmail.com>
2019-05-10 16:43:27 +0700

    more modeling

--- scratch/form_model.py
+++ scratch/form_model.py
@@ -8,17 +8,35 @@ class Form():
         self.image = image # path to image
         self.questions = questions # list of Question
 
+    def to_JSON(self):
+        dict_repr = self.__dict__
+        dict_repr["__type__"] = Form.__name__
+        dict_repr["questions"] = [q.to_JSON() for q in self.questions]
+        return dict_repr
+
 class Question():
     def __init__(self, name, type, responses):
         self.name = name # name of column in data table
         self.type = type # type of question, ex. checkbox, radio button
         self.responses = responses
 
+    def to_JSON(self):
+        dict_repr = self.__dict__
+        dict_repr["__type__"] = Question.__name__
+        dict_repr['responses'] = [r.to_JSON() for r in self.responses]
+        return dict_repr
+
 class Response():
     def __init__(self, name, location, background):
         self.name = name # meaning of the cell
         self.location =  location # Location; location within the template image
         self.background = background # array of background pixels within the template
+
+    def to_JSON(self):
+        dict_repr = self.__dict__
+        dict_repr["__type__"] = Response.__name__
+        dict_repr['location'] = self.location.to_JSON()
+        return dict_repr
 
 class Location():
     def __init__(self, x, y, width, height):
@@ -27,48 +45,54 @@ class Location():
         self.width = width # width of bounding rectangle
         self.height = height # height of bounding rectangle
 
+    def to_JSON(self):
+        dict_repr = self.__dict__
+        dict_repr["__type__"] = Location.__name__
+        return dict_repr
+
+
 ### JSON encoder / decoder ###
 # Returns a dict that can be serialized to JSON
-def encode(py_class):
-    # Validate input
-    valid_encode_type = (Form, Question, Response, Location)
-    if type(py_class) not in valid_encode_type:
-        raise Exception("Unable to encode input %s: Expected a Form, Question, Response, or Location" % str(py_class))
-
-    dict = py_class.__dict__
-    dict["__type__"] = type(py_class).__name__
-    if isinstance(py_class, Form):
-        encoded_questions = [encode(question) for question in py_class.questions]
-        dict["questions"] = encoded_questions
-        return dict
-    elif isinstance(py_class, Question):
-        encoded_responses = [encode(response) for response in py_class.responses]
-        dict["responses"] = encoded_responses
-        return dict
-    elif isinstance(py_class, Response):
-        encoded_location = encode(py_class.location)
-        dict["location"] = encoded_location
-        return dict
-    elif isinstance(py_class, Location):
-        return dict
-    else:
-        raise Exception("Invalid input class for JSON encoding.")
+# def encode(py_class):
+#     # Validate input
+#     valid_encode_type = (Form, Question, Response, Location)
+#     if type(py_class) not in valid_encode_type:
+#         raise Exception("Unable to encode input %s: Expected a Form, Question, Response, or Location" % str(py_class))
+#
+#     dict = py_class.__dict__
+#     dict["__type__"] = type(py_class).__name__
+#     if isinstance(py_class, Form):
+#         encoded_questions = [encode(question) for question in py_class.questions]
+#         dict["questions"] = encoded_questions
+#         return dict
+#     elif isinstance(py_class, Question):
+#         encoded_responses = [encode(response) for response in py_class.responses]
+#         dict["responses"] = encoded_responses
+#         return dict
+#     elif isinstance(py_class, Response):
+#         encoded_location = encode(py_class.location)
+#         dict["location"] = encoded_location
+#         return dict
+#     elif isinstance(py_class, Location):
+#         return dict
+#     else:
+#         raise Exception("Invalid input class for JSON encoding.")
 
 # Returns an object of type Form
 def decode(json):
-    if '__type__' in json and json['__type__'] == type(Form).__name__:
-        questions = [decode(question) for question in json.questions]
+    if '__type__' in json and json['__type__'] == Form.__name__:
+        questions = [decode(question) for question in json["questions"]]
         return Form(json['name'], json['image'], questions)
-    elif '__type__' in json and json['__type__'] == type(Question).__name__:
-        responses = [decode(response) for response in json.responses]
+    elif '__type__' in json and json['__type__'] == Question.__name__:
+        responses = [decode(response) for response in json["responses"]]
         return Question(json['name'], json['type'], responses)
-    elif '__type__' in json and json['__type__'] == type(Response).__name__:
+    elif '__type__' in json and json['__type__'] == Response.__name__:
         location = decode(json['location'])
         return Response(json['name'], location, json['background'])
-    elif '__type__' in json and json['__type__'] == type(Location).__name__:
+    elif '__type__' in json and json['__type__'] == Location.__name__:
         return Location(json['x'], json['y'], json['width'], json['height'])
     else:
-        raise Exception("Unable to convert JSON to internal Form model; No recognized __type__ field")
+        raise Exception("Unable to convert JSON to internal Form model; No recognized __type__ field; %s" % json)
 
 
 def from_dict(d):
@@ -78,10 +102,8 @@ loc1 = Location(1, 2, 3, 4)
 loc2 = Location(2, 4, 6, 8)
 r1 = Response("R1", loc1, [])
 r2 = Response("R2", loc2, [])
-q1 = Question("Q1", "Radio Button", [r1, r2])
-q2 = Question("Q2", "Checkbox", [r1, r2])
+q1 = Question("Q1", "Radio Button", [r1])
+q2 = Question("Q2", "Checkbox", [r2])
 f = Form("anc template", "example/phone_pics/images", [q1, q2])
 
-print(Form.__name__)
-print(f)
-print(encode(f))
+print(decode(f.to_JSON()).to_JSON())
