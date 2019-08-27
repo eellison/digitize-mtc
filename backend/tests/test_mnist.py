3aad50f8c9149b8c8aa2c631a04dab5453d878b1
Elias Ellison <eellison@fb.com>
2019-08-26 23:07:50 -0700

    initial mnist capability

---
+++ backend/tests/test_mnist.py
@@ -0,0 +1,31 @@
+
+import sys
+sys.path.append("..") # Adds higher directory to python modules path so we can import
+import os
+from scripts import predict_digit
+
+def test():
+
+    # these are digits taken from a scanned filled out form, so they should be
+    # somewhat accurate to what we will receive.
+    # currently the border of the digit grid is cropped out
+    images = [
+        ("zero_screenshot.png", 0),
+        ("one_screenshot.png", 1),
+        ("two_screenshot.png", 2),
+        ("three_screenshot.png", 3),
+    ]
+
+    num_predicted = 0
+    for image_name, expected_digit in images:
+        image_path = os.path.abspath("./mnist_digits/" + image_name)
+        prediction, prob = predict_digit(image_path)
+
+        # we should either have the prediction right or have low confidence
+        assert prediction == expected_digit or prob <= .8
+        num_predicted += (prediction == expected_digit)
+
+    print("{predicted} predicted out of {total}".format(predicted=num_predicted, total=len(images)))
+
+if __name__ == "__main__":
+    test()
