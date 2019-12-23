a6cea202ac841974b79cf9d1341ffea4d5cbfc00
sdrp <sudheesha_perera@brown.edu>
2019-12-23 11:54:40 +0700

    disable mnist digit detection, for now

--- backend/scripts/__init__.py
+++ backend/scripts/__init__.py
@@ -4,6 +4,6 @@ from .form import *
 from .util import *
 from .encoder import *
 from .process import *
-from .mnist import predict_digit
+# from .mnist import predict_digit
 from .digit_segmentation import extract_digit_boxes
 from .detect_blurriness import compute_blurriness
